955d0dd77bb5343a30a1f34b1d02b324a60cd08d
Nazal-nihad <118171350+Nazal-nihad@users.noreply.github.com>
2023-01-03 14:52:04 +0530

    Create pascal2.cpp

---
+++ pascal2.cpp
@@ -0,0 +1,54 @@
+//program to print the given no of lines as a pascals triangle with spaces
+
+#include <iostream>
+using namespace std;
+
+int ncr(int n); //ncr function declared
+int ncr(int n) //ncr function defined
+{
+    int p = 1;
+    while(n>=1)
+    {
+    p=p*n;
+    n=n-1;
+    }
+    return p;
+}
+
+int factorial(int a,int b); //factorial function declared
+int factorial(int a,int b) //factorial function defined
+{
+    int x=ncr(a);
+    int y=ncr(b);
+    int c=ncr((a-b));
+    int result = x/(y*c); 
+    
+    return result;
+}
+
+int main()
+{    
+int n,r,j,i;
+i=0;
+
+cout<<"enter the number of lines: ";
+cin>>n;
+
+while(i<=n-1) //looping starts for line 
+{
+    r=0;
+  
+    for(j=1;j<n-i;j++) //loop for spaces
+    {
+        std::cout<<" ";
+    }
+    while(r<=i) //loop for row
+    {
+        cout<<factorial(i,r)<<" ";
+        r++;
+    }
+    cout<<"\n";
+    i++;
+}
+return 0;
+}
